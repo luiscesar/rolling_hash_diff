a0ae54ef436089f45a64391d0a0cccc8fce2617e
Luis Cesar Maiaru <luiscesar.mayaru@gmail.com>
2022-08-13 17:48:42 +0100

    Tests updated.

--- tests/rdiff/mod.rs
+++ tests/rdiff/mod.rs
@@ -1,13 +1,15 @@
 use rolling_hash_diff::rdiff::Rdiff;
 
+const COMMAND:&str = "rolling_hash_diff";
+
 #[test]
-fn test_rdiff_main_signature_case1() {
+fn integration_test_rdiff_main_signature_case1() {
+    // Create signature
     let file_name = "resources/test.txt";
     let signature_file_name = "resources/test.sig";
     let option = "signature";
-    let command = "rdiff-rolling-hash";
     let mut args:Vec<String> = Vec::new();
-    args.push(command.to_string());
+    args.push(COMMAND.to_string());
     args.push(option.to_string());
     args.push(file_name.to_string());
     args.push(signature_file_name.to_string());
@@ -15,14 +17,24 @@ fn test_rdiff_main_signature_case1() {
 }
 
 #[test]
-fn test_rdiff_main_delta_case1() {
-    let file_name = "resources/test.v2.txt";
-    let signature_file_name = "resources/test.sig";
-    let delta_file_name = "resources/test.sig";
-    let option = "delta";
-    let command = "rdiff-rolling-hash";
+fn integration_test_rdiff_main_delta_case1() {
+    // Create signature
+    let file_name = "resources/test.txt";
+    let signature_file_name = "resources/test_delta.sig";
+    let option = "signature";
     let mut args:Vec<String> = Vec::new();
-    args.push(command.to_string());
+    args.push(COMMAND.to_string());
+    args.push(option.to_string());
+    args.push(file_name.to_string());
+    args.push(signature_file_name.to_string());
+    let rdiff_main_result = Rdiff::main_rdiff(args).unwrap();
+    // Create delta
+    let file_name = "resources/test.v2.txt";
+    let signature_file_name = "resources/test_delta.sig";
+    let delta_file_name = "resources/test.delta";
+    let option = "delta";
+    let mut args:Vec<String> = Vec::new();
+    args.push(COMMAND.to_string());
     args.push(option.to_string());
     args.push(signature_file_name.to_string());
     args.push(file_name.to_string());
