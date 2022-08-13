cc650cdeeba68c46972be1e2fe68495f8f16eba4
Luis Cesar Maiaru <luiscesar.mayaru@gmail.com>
2022-08-13 17:32:46 +0100

    Error messages updated.

--- src/rdiff/error/messages.rs
+++ src/rdiff/error/messages.rs
@@ -1,4 +1,4 @@
 pub const FILE_NOT_FOUND:&str = "File not found";
 pub const HELP_USAGE:&str = 
-    "USAGE:\nrdiff-rolling-hash signature <file-name> <signature-file-name>\nrdiff-rolling-hash delta <signature-file-name> <new-file-name> <delta-file-name>";
+    "USAGE:\nrrolling_hash_diff signature <file-name> <signature-file-name>\nrolling_hash_diff delta <signature-file-name> <new-file-name> <delta-file-name>";
 pub const INVALID_CHUNK_SIZE:&str = "Chunk size should be less than or equal to block size";    
