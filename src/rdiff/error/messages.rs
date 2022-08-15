7a89c6f61e4509b1b43f361c798f5bbe6e301984
Luis Cesar Maiaru <luiscesar.mayaru@gmail.com>
2022-08-15 06:53:00 +0100

    Signature type, integration tests. Error test cases added.

--- src/rdiff/error/messages.rs
+++ src/rdiff/error/messages.rs
@@ -1,4 +1,4 @@
-pub const FILE_NOT_FOUND:&str = "File not found";
+pub const FILE_NOT_FOUND:&str = "The system cannot find the file specified";
 pub const HELP_USAGE:&str = 
     "USAGE:\nrolling_hash_diff signature <file-name> <signature-file-name>\nrolling_hash_diff delta <signature-file-name> <new-file-name> <delta-file-name>";
 pub const INVALID_CHUNK_SIZE:&str = "Chunk size should be less than or equal to block size";  
--- tests/rdiff/signature/mod.rs
+++ tests/rdiff/signature/mod.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::{BufWriter, Write}};
 
-use rolling_hash_diff::rdiff::{Rdiff, constants::BLOCK_SIZE};
+use rolling_hash_diff::rdiff::{Rdiff, constants::BLOCK_SIZE, error::messages::{HELP_USAGE, FILE_NOT_FOUND}};
 
 use crate::rdiff::{COMMAND, util::now_as_millis};
 
@@ -63,4 +63,61 @@ pub fn integration_test_rdiff_main_signature_case2() {
 
     // Verify result
     assert_eq!(rdiff_main_result, ());
+}
+
+#[test]
+fn integration_test_rdiff_main_signature_error_no_option_case1() {
+    // Get file names
+    let file_name = format!("resources/test_signature_error_case1.{}.txt",now_as_millis());
+    let signature_file_name = format!("{}.sig", file_name);
+
+    // Execute command
+   let option = "signature";
+    let mut args:Vec<String> = Vec::new();
+    args.push(COMMAND.to_string());
+    //args.push(option.to_string());
+    args.push(file_name.to_string());
+    args.push(signature_file_name.to_string());
+    let rdiff_main_result = Rdiff::main_rdiff(args).unwrap_err();
+    
+    // Verify result
+    assert_eq!(rdiff_main_result.to_string(), HELP_USAGE);
+}
+
+#[test]
+fn integration_test_rdiff_main_signature_error_file_name_missing_case2() {
+   // Get file names
+    let file_name = format!("resources/test_signature_error_case2.{}.txt",now_as_millis());
+    let signature_file_name = format!("{}.sig", file_name);
+
+    // Execute command
+   let option = "signature";
+    let mut args:Vec<String> = Vec::new();
+    args.push(COMMAND.to_string());
+    args.push(option.to_string());
+    //args.push(file_name.to_string());
+    args.push(signature_file_name.to_string());
+    let rdiff_main_result = Rdiff::main_rdiff(args).unwrap_err();
+    
+    // Verify result
+    assert_eq!(rdiff_main_result.to_string(), HELP_USAGE);
+}
+
+#[test]
+fn integration_test_rdiff_main_signature_error_file_missing_case3() {
+   // Get file names
+    let file_name = format!("resources/test_signature_error_case3.{}.txt",now_as_millis());
+    let signature_file_name = format!("{}.sig", file_name);
+
+    // Execute command
+   let option = "signature";
+    let mut args:Vec<String> = Vec::new();
+    args.push(COMMAND.to_string());
+    args.push(option.to_string());
+    args.push(file_name.to_string());
+    args.push(signature_file_name.to_string());
+    let rdiff_main_result = Rdiff::main_rdiff(args).unwrap_err();
+    
+    // Verify result
+    assert!(rdiff_main_result.to_string().contains(FILE_NOT_FOUND));
 }
